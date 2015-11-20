b5034121bfb14085670f33cc4db39de289b104d7
ymmtmsys <m2yamamoto@gmail.com>
2015-11-20 22:17:44 +0900

    Impl init/1 and call/2

--- lib/plug_teapot.ex
+++ lib/plug_teapot.ex
@@ -1,2 +1,13 @@
 defmodule PlugTeapot do
+  alias Plug.Conn
+  alias Plug.Conn.Status
+
+  @behaviour Plug
+
+  def init(default), do: default
+
+  def call(conn, _) do
+      conn
+      |> Conn.put_status Status.code(:im_a_teapot)
+  end
 end
