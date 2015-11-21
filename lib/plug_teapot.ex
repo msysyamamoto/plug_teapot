c5927fbdb418194bd7b85a9d5664f0b5e9511225
Yamamoto Masayasu <m2yamamoto@gmail.com>
2015-11-22 08:12:57 +0900

    Fmt

--- lib/plug_teapot.ex
+++ lib/plug_teapot.ex
@@ -1,26 +1,26 @@
 defmodule PlugTeapot do
-  import Plug.Conn, only: [put_status: 2, put_resp_header: 3, send_resp: 3, halt: 1]
-  alias Plug.Conn.Status
+    import Plug.Conn, only: [put_status: 2, put_resp_header: 3, send_resp: 3, halt: 1]
+    alias Plug.Conn.Status
 
-  @behaviour Plug
+    @behaviour Plug
 
-  def init(default), do: default
+    def init(default), do: default
 
-  def call(conn, opts) do
-      conn
-      |> put_status(Status.code(:im_a_teapot))
-      |> resp(opts)
-  end
+    def call(conn, opts) do
+        conn
+        |> put_status(Status.code(:im_a_teapot))
+        |> resp(opts)
+    end
 
-  defp resp(conn, opts) do
-      case Keyword.fetch(opts, :custom_resp) do
-          {:ok, true} ->
-              conn
-          _ ->
-              conn
-              |> put_resp_header("content-type", "text/plain")
-              |> send_resp(conn.status, "I'm a teapot")
-              |> halt
-      end
-  end
+    defp resp(conn, opts) do
+        case Keyword.fetch(opts, :custom_resp) do
+            {:ok, true} ->
+                conn
+            _ ->
+                conn
+                |> put_resp_header("content-type", "text/plain")
+                |> send_resp(conn.status, "I'm a teapot")
+                |> halt
+        end
+    end
 end
